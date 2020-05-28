f35cdab6679469d4c2d7a62bde1b2c4887bedb37
ms2jpg <28683657+ms2jpg@users.noreply.github.com>
2020-05-28 21:00:07 +0200

    plecakowy

---
+++ knapsack/test.py
@@ -0,0 +1,27 @@
+from knapsack import *
+from terminaltables import AsciiTable
+
+
+
+
+# brutalforce
+# string="""4 8
+# 2 4
+# 1 3
+# 4 6
+# 4 8"""
+
+# multi dynamic
+string="""5 7
+2 4
+1 3
+4 6
+4 8
+3 7"""
+
+
+
+
+xd = Knapsack.load(string)
+print(xd)
+print(Knapsack.printBackpack(xd.dynamicAlgorithm()))
